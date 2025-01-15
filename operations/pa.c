6d17ee910926b5678a47222502d26d21be1f1be4
N0ViP <01.xx.vip@gmail.com>
2025-01-15 15:52:26 +0100

    fix pa, pb

--- operations/pa.c
+++ operations/pa.c
@@ -6,19 +6,19 @@
 /*   By: yjaafar <yjaafar@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 18:29:33 by yjaafar           #+#    #+#             */
-/*   Updated: 2025/01/14 18:49:12 by yjaafar          ###   ########.fr       */
+/*   Updated: 2025/01/15 15:52:11 by yjaafar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void pa(t_list lista, t_list listb)
+void	pa(t_list **lista, t_list **listb)
 {
-	t_list tmp;
-	if (!listb)
+	t_list *tmp;
+	if (!lista || *listb || !*listb)
 		return ;
-	tmp = lista;
-	lista = listb;
-	listb = listb->next;
-	lista->next = tmp;
+	tmp = *lista;
+	*lista = *listb;
+	*listb = (*listb)->next;
+	(*lista)->next = tmp;
 }
--- operations/pb.c
+++ operations/pb.c
@@ -6,19 +6,19 @@
 /*   By: yjaafar <yjaafar@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 18:40:40 by yjaafar           #+#    #+#             */
-/*   Updated: 2025/01/14 18:49:10 by yjaafar          ###   ########.fr       */
+/*   Updated: 2025/01/15 15:51:53 by yjaafar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	pb(t_list lista, t_list listb)
+void	pb(t_list **listb, t_list **lista)
 {
-	t_list tmp;
-	if (!lista)
+	t_list *tmp;
+	if (!listb || *lista || !*lista)
 		return ;
-	tmp = listb;
-	listb = lista;
-	listb = listb->next;
-	lista->next = tmp;
+	tmp = *listb;
+	*listb = *lista;
+	*lista = (*lista)->next;
+	(*listb)->next = tmp;
 }
