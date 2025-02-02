68149a09fa94ea6e2b4262fcc33752ad8059b6c4
N0ViP <01.xx.vip@gmail.com>
2025-02-02 06:57:54 +0100

    break fast

--- src/ft_lstadd_back_bonus.c
+++ src/ft_lstadd_back_bonus.c
@@ -1,34 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yjaafar <yjaafar@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/14 18:02:31 by yjaafar           #+#    #+#             */
-/*   Updated: 2025/02/02 06:33:56 by yjaafar          ###   ########.fr       */
+/*   Created: 2025/02/02 06:31:30 by yjaafar           #+#    #+#             */
+/*   Updated: 2025/02/02 06:32:08 by yjaafar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#include "push_swap.h"
 
-# include <unistd.h>
-# include <stdlib.h>
-#include <stdio.h>
-//# include "ft_printf.h"
-
-# define LIST_MAX 500
-
-typedef struct	s_list
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	int				val;
-	int				idx;
-	struct s_list*	next;
-}	t_list;
+	t_list	*tmp;
 
-int	ft_isdigit(int c);
-int	ft_isspace(int c);
-void    ft_lstadd_back(t_list **lst, t_list *new);
-
-#endif
+	tmp = *lst;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	while (tmp->next)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = new;
+}
--- src/main.c
+++ src/main.c
@@ -1,30 +1,35 @@
 #include "push_swap.h"
 
-int	ft_one_arg(t_list *a, char *av)
+int	ft_one_arg(t_list **A, t_list *a, char *av)
 {
 	static int	aSize;
 	long long	tmp;
+	int			sign;
 
-	tmp = 0;
 	while (*av)
 	{
+		sign = 0;
 		while (*av == ' ' || (*av >= 9 && *av <= 13))
 			av++;
+		tmp = 0;
+		sign = (*av == '-') * -1 + (*av != '-');
+		av += (*av == '-' || *av == '+');
 		while (*av >= 48 && *av <= 57)
 		{
 			tmp = (tmp << 3) + (tmp << 1) + (*av++ & 0X0F);
-			if (tmp > 2147483647 || tmp < -2147483648)
+			if (tmp > 2147483647 || tmp <= -1)
 				return (-1);
 		}
-		a[aSize].val = tmp;
-		a[aSize].idx = aSize++;
+		a[aSize].val = tmp * sign;
+		a[aSize].idx = aSize;
+		ft_lstadd_back(A, &a[aSize++]);
 		if (*av && !ft_isspace(*av) && !ft_isdigit(*av))
 			return (-1);
 	}
 	return (aSize);
 }
 
-int	ft_multi_args(t_list *a, char **av)
+int	ft_multi_args(t_list **A, t_list *a, char **av)
 {
 	int	i;
 	int	aSize;
@@ -33,7 +38,7 @@ int	ft_multi_args(t_list *a, char **av)
 	aSize = 0;
 	while (av[i])
 	{
-		aSize = ft_one_arg(a, av[i]);
+		aSize = ft_one_arg(A, a, av[i]);
 		if (aSize == -1)
 			return (-1);
 		i++;
@@ -41,28 +46,32 @@ int	ft_multi_args(t_list *a, char **av)
 	return (aSize);
 }
 
-int	ft_fill_list_a(t_list *a, char **av, int ac)
+int	ft_fill_list_a(t_list **A, t_list *a, char **av, int ac)
 {
 	int	aSize;
 
 	if (ac == 1)
-		aSize = ft_one_arg(a, av[0]);
+		aSize = ft_one_arg(A, a, av[0]);
 	else
-		aSize = ft_multi_args(a, av);
+		aSize = ft_multi_args(A, a, av);
 	return (aSize);
 }
 
 int main(int ac, char *av[])
 {
-	int		aSize;
-	t_list	a[LIST_MAX];
-	t_list	b[LIST_MAX];
+	int				aSize;
+	static t_list	tmp[LIST_MAX];
+	t_list			*A;
 
+	A = NULL;
 	if (ac == 1 || (ac = 2 && !av[1][0]))
 		return (1);
-	aSize = ft_fill_list_a(&a, av + 1, ac - 1);
-	for (int i = 0; i < aSize; i++)
+	aSize = ft_fill_list_a(&A, tmp, av + 1, ac - 1);
+	if (aSize == -1)
+		return (printf("Error\n"));
+	while (A)
 	{
-		printf("a[%d] = %d\n", i, a[i].val);
+		printf("a[%d] = %d\n", A->idx, A->val);
+		A = A->next;
 	}
 }
