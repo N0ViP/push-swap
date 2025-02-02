20edbce26d44eb401dd128574e8c94f32cd534e3
N0ViP <01.xx.vip@gmail.com>
2025-02-02 06:09:41 +0100

    starting reading the inputs

--- src/main.c
+++ src/main.c
@@ -1,34 +1,68 @@
 #include "push_swap.h"
 
-int	ft_fill_list_a(t_list *a, char **av)
+int	ft_one_arg(t_list *a, char *av)
 {
-	long	tmp;
+	static int	aSize;
+	long long	tmp;
+
+	tmp = 0;
+	while (*av)
+	{
+		while (*av == ' ' || (*av >= 9 && *av <= 13))
+			av++;
+		while (*av >= 48 && *av <= 57)
+		{
+			tmp = (tmp << 3) + (tmp << 1) + (*av++ & 0X0F);
+			if (tmp > 2147483647 || tmp < -2147483648)
+				return (-1);
+		}
+		a[aSize].val = tmp;
+		a[aSize].idx = aSize++;
+		if (*av && !ft_isspace(*av) && !ft_isdigit(*av))
+			return (-1);
+	}
+	return (aSize);
+}
+
+int	ft_multi_args(t_list *a, char **av)
+{
 	int	i;
+	int	aSize;
 
 	i = 0;
+	aSize = 0;
 	while (av[i])
 	{
-		tmp = ft_atol(av[i]);
-		if (tmp > MAX_INT || tmp < MIN_INT || av[i])
+		aSize = ft_one_arg(a, av[i]);
+		if (aSize == -1)
 			return (-1);
-		av[i].val = tmp;
-		av[i].idx = i;
 		i++;
 	}
+	return (aSize);
+}
 
-	return (0);
+int	ft_fill_list_a(t_list *a, char **av, int ac)
+{
+	int	aSize;
+
+	if (ac == 1)
+		aSize = ft_one_arg(a, av[0]);
+	else
+		aSize = ft_multi_args(a, av);
+	return (aSize);
 }
 
 int main(int ac, char *av[])
 {
+	int		aSize;
 	t_list	a[LIST_MAX];
 	t_list	b[LIST_MAX];
 
-	if (ac == 1 || (ac = 2 && !av[1][i]))
+	if (ac == 1 || (ac = 2 && !av[1][0]))
 		return (1);
-	if (ac == 2)
-		av = ft_split(av[1]);
-	ft_fill_list_a(&a, av + 1);
-	ft_move_to_b();
-	ft_move_to_a();
+	aSize = ft_fill_list_a(&a, av + 1, ac - 1);
+	for (int i = 0; i < aSize; i++)
+	{
+		printf("a[%d] = %d\n", i, a[i].val);
+	}
 }
---
+++ src/push_swap.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yjaafar <yjaafar@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/14 18:02:31 by yjaafar           #+#    #+#             */
+/*   Updated: 2025/02/02 06:06:22 by yjaafar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <unistd.h>
+# include <stdlib.h>
+#include <stdio.h>
+//# include "ft_printf.h"
+
+# define LIST_MAX 500
+
+typedef struct	s_list
+{
+	int				val;
+	int				idx;
+}	t_list;
+
+int	ft_isdigit(int c);
+int	ft_isspace(int c);
+
+#endif
