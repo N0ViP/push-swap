88e485791c44838ee078bc38b8e4cfd8cb1f30ed
N0ViP <01.xx.vip@gmail.com>
2025-02-09 18:54:35 +0100

    optimizing the code

--- src/ft_move_to_a.c
+++ src/ft_move_to_a.c
@@ -6,7 +6,7 @@
 /*   By: yjaafar <yjaafar@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/08 09:02:50 by yjaafar           #+#    #+#             */
-/*   Updated: 2025/02/09 17:53:44 by yjaafar          ###   ########.fr       */
+/*   Updated: 2025/02/09 18:54:28 by yjaafar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,64 @@
 
 int	ft_get_target(int n, t_list *list_a)
 {
+	
+}
+
+int ft_get_move_a(int n, t_list **list_b->val)
+{
 	int	target;
 
 	target = INT_MAX;
 
 	while (list_a)
 	{
-		if (list_a->val < target && n < list_a->val)
-		{
-			target = list_a->val;
-		}
-		list_a = list_a->next;
+	if (list_a->val < target && n < list_a->val)
+	{
+		target = list_a->val;
 	}
+		list_a = list_a->next;
+	}   
 	return (target);
 }
 
-void ft_move_element(t_list **list_a, t_list **list_b, int target)
+int	ft_get_move_b(int n, t_list **list_b)
 {
-	while (*list_a && (*list_a)->val != target)
+	int	val;
+
+	val = INT_MIN;
+	while (list_b)
 	{
-		ra(list_a);
-		printf("ra\n");
+		if (list_b->val < n && val < list_b->val)
+		{
+			val = list_b->val;
+		}
+		list_b = list_b->next;
 	}
-	printf("pa\n");
-	pa(list_a, list_b);
+	return (val);
+}
+
+int	ft_move_dep(int n, t_list *list_b)
+{
+	int	i;
+	int	val;
+
+	i = 0;
+	val = ft_get_dep(n, list_b);
+	while (list_b && list_b->val != val)
+	{
+		list_b = list_b->next;
+		i++;
+	}
+	return (i);
+}
+
+void	ft_best_move(t_list **list_a, t_list **list_b, int asize)
+{
+	int	a;
+	int	b;
+
+	a = ft_move_target((*list_b)->val, list_a);
+	b = ft_move_dep((*list_a)->val, list_b);
 }
 
 void	ft_move_to_a(t_list **list_a, t_list **list_b, int asize)
@@ -46,7 +80,7 @@ void	ft_move_to_a(t_list **list_a, t_list **list_b, int asize)
 
 	while (*list_b)
 	{
-		target = ft_get_target((*list_b)->val, *list_a);
+		ft_best_move(list_a, list_b, asize);
 		ft_move_element(list_a, list_b, target);
 	}
 }
